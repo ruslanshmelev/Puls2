821e109aef26af851db6c7dac71ddf20402d839e
Ruslan <shmelevruslan@rambler.ru>
2024-04-11 00:37:58 +0300

    CatalogEnd

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,28 @@ $(document).ready(function(){
 			},
 		]
 });
+
+/*Табы*/
+$('ul.tabs__caption').on('click', 'li:not(.tabs__caption__tab_active)', function() {
+    $(this)
+      .addClass('tabs__caption__tab_active').siblings().removeClass('tabs__caption__tab_active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('tabs__content_active').eq($(this).index()).addClass('tabs__content_active');
+  });
+/*оборотная сторонс*/
+function toggleSlide(item) {
+	$(item).each(function(i) {
+		$(this).on('click', function(e) {
+			e.preventDefault();
+			$('.tabs-item__dscp').eq(i).toggleClass('tabs-item__dscp_active');
+			$('.tabs-item__list').eq(i).toggleClass('tabs-item__list_active');
+		})
+	});
+};
+
+toggleSlide('.tabs-item__dscp__det');
+toggleSlide('.tabs-item__list__back');
+
+	
+
+
 });
